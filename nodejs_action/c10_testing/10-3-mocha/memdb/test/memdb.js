ce6c86d37432f045a1cd947c545615ada215fd36
annie <a.guo.2010@gmail.com>
2014-06-13 22:46:07 +0000

    added more test cases

--- nodejs_action/c10_testing/10-3-mocha/memdb/test/memdb.js
+++ nodejs_action/c10_testing/10-3-mocha/memdb/test/memdb.js
@@ -5,26 +5,41 @@ describe('memdb', function(){
   // beforeEach
   beforeEach(function(){
     memdb.clear()
-  });
+  }); // beforeEach
 
   // test save
   describe('.save(doc)', function(){  
     it('shoud save the document', function(){
-      var pet = {name: 'Tobi'}
-      memdb.save(pet)
-      var ret = memdb.first({name:'Tobi'})
-      assert(ret == pet);
-    })
-  });
+      var tobi = {name: 'Tobi'}
+      var loki = {name: 'Loki'}
+      memdb.save(tobi)
+      memdb.save(loki)
 
-});
+      var ret = memdb.first({name:'Tobi'})
+      assert(ret == tobi);
+
+      var ret = memdb.first({name:'Loki'})
+      assert(ret == loki);
+    })
+
+    it('shoud return null when no doc match', function(){
+      var manny = {name: 'Manny'}
+      var ret = memdb.first({name:'Loki'})
+      assert(ret == null);
+    })
+
+  }); // describe save
+
+}); // describe memdb
 
 
 // doc
 //
 //  asset - http://visionmedia.github.io/mocha/#assertions
 //  bdd interface - http://visionmedia.github.io/mocha/#interfaces
-//
+//  hooks - http://visionmedia.github.io/mocha/
+//    All "hooks", that is before(), after(), beforeEach(), afterEach() may be sync or async as well, 
+//    behaving much like a regular test-case. For example you may wish to populate database with dummy content before each test
 //
 // npm
 //  https://www.npmjs.org/package/mocha 
