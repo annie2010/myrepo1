bb8db9d0519bf93168a5a8605f3dd6e670d475ce
annie <a.guo.2010@gmail.com>
2014-06-13 22:39:43 +0000

    added setup

--- nodejs_action/c10_testing/10-3-mocha/memdb/index.js
+++ nodejs_action/c10_testing/10-3-mocha/memdb/index.js
@@ -4,6 +4,10 @@ exports.save = function(f_entry){
   db.push(f_entry)
 }
 
+exports.clear = function(){
+  db = []
+}
+
 exports.first=function(f_obj){
   return db.filter(function(entry){ // return a entry with all fields match the request obj
     for(var key in f_obj){
--- nodejs_action/c10_testing/10-3-mocha/memdb/test/memdb.js
+++ nodejs_action/c10_testing/10-3-mocha/memdb/test/memdb.js
@@ -2,6 +2,12 @@ var memdb = require('..')
 var assert = require('assert')
 
 describe('memdb', function(){
+  // beforeEach
+  beforeEach(function(){
+    memdb.clear()
+  });
+
+  // test save
   describe('.save(doc)', function(){  
     it('shoud save the document', function(){
       var pet = {name: 'Tobi'}
@@ -9,5 +15,21 @@ describe('memdb', function(){
       var ret = memdb.first({name:'Tobi'})
       assert(ret == pet);
     })
-  })
+  });
+
 });
+
+
+// doc
+//
+//  asset - http://visionmedia.github.io/mocha/#assertions
+//  bdd interface - http://visionmedia.github.io/mocha/#interfaces
+//
+//
+// npm
+//  https://www.npmjs.org/package/mocha 
+//
+//
+// limitation
+//   Mocha tests run serially
+//   making asynchronous testing simple and fun
