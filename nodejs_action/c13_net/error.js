d8c048bed773ec9ea57364363e0ca8d0b4ebaa34
annie <a.guo.2010@gmail.com>
2014-06-16 22:48:53 +0000

    try-catch-finally and thow

--- nodejs_action/c13_net/error.js
+++ nodejs_action/c13_net/error.js
@@ -25,9 +25,40 @@ var done = function(f_err, f_ret){
   console.log(f_ret)
 }
 
+console.log("\n\n== Drive 1, callback with err or not")
 isTrue(false, done)
 isTrue(true, done)
 
-console.log("=====My program ends successfually")
+
+console.log("\n\n== Drive 2, error object")
+var error = new Error("The error message");
+console.log(error);
+console.log(error.stack);
+
+console.log("\n\n== Drive 3, try catch")
+try{
+  console.log("Enter try-block")
+  throw "thrown message"
+  console.log("Exit try-block")
+}
+catch(e){
+  console.log("Enter catch-block")
+  console.log("CATCH : ",e)
+  console.log("Exit catch-block")
+}
+finally{
+  console.log("Enter finally-block")
+  console.log("Exit finally-block")
+}
+console.log("== Drive 3, try catch, done")
+
+
+console.log("\n\n=====My program ends successfually")
+
 // ref
+//
 // http://docs.nodejitsu.com/articles/errors/what-are-the-error-conventions
+//
+// http://docs.nodejitsu.com/articles/errors/what-is-the-error-object
+//
+// http://docs.nodejitsu.com/articles/errors/what-is-try-catch
