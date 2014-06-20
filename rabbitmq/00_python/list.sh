2f2d7cda35251948049070f607d5cac17f6c1275
annie <a.guo.2010@gmail.com>
2014-06-20 17:12:56 +0000

    rabbitmq python binding via pika

---
+++ rabbitmq/00_python/list.sh
@@ -0,0 +1,1 @@
+sudo rabbitmqctl list_exchanges; sudo rabbitmqctl list_bindings; sudo rabbitmqctl list_queues
---
+++ rabbitmq/00_python/reset.sh
@@ -0,0 +1,6 @@
+rabbitmqctl stop
+sudo invoke-rc.d rabbitmq-server restart
+
+rabbitmqctl stop_app
+rabbitmqctl reset
+rabbitmqctl start_app
